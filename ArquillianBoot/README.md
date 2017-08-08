19120384aca66cc14e6ae0595999e21b007909e4
Jakub Krhovjak <jakub.krhovjak@protonmail.com>
2017-08-08 18:02:43 +0200

    Changed directory structure

--- ArquillianBoot/README.md
+++
@@ -1,2 +0,0 @@
-# ArquillianBoot
-dd
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,11 +12,6 @@ import java.util.Map;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		final Map<String, Object> map = new HashMap<>();
-		map.put("endpoints.shutdown.enabled", true);
-		map.put("endpoints.shutdown.sensitive", false);
-		new SpringApplicationBuilder(DemoApplication.class).listeners(new ApplicationPidFileWriter("app.pid"))
-				.logStartupInfo(true).properties(map).run(args);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
